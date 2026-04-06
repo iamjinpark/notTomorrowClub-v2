01ece7fce32f2aa2ff836b69192995a0a073b004
iamjinpark <wldls7754@gmail.com>
2026-04-06 09:53:08 +0900

    [fix] 단어 다운로드 컴포넌트 숨김처리 및 폰트 크기 수정

--- src/components/review/ReviewWordsForCapture.jsx
+++ src/components/review/ReviewWordsForCapture.jsx
@@ -2,21 +2,17 @@ import wordIcon from "@/assets/img/wordIcon.svg";
 
 function ReviewWordsForCapture({ ref, allWords }) {
   return (
-    <div className="fixed left-0 top-0 z-50 overflow-auto" ref={ref}>
+    <div className="fixed left-[-9999px] top-0" ref={ref}>
       <div className="bg-lightyellow flex items-center justify-center w-[1080px] h-[1350px] py-[315px] px-[290px]">
-        <div className="h-full ₩flex flex-col items-start">
+        <div className="flex flex-col items-start gap-[20px]">
           {allWords.map((word, index) => (
-            <div key={index} className="flex items-center gap-[20px]">
-              <img
-                src={wordIcon}
-                alt=""
-                className="w-[14px] h-[14px] shrink-0"
-              />
-              <div className="flex items-baseline gap-[28px]">
-                <span className="font-roboto font-normal text-[52px] leading-[125%]">
+            <div key={index} className="flex items-center gap-[16px]">
+              <img src={wordIcon} alt="" className="w-[10px] h-[10px] shrink-0" />
+              <div className="flex items-baseline gap-[20px]">
+                <span className="font-roboto font-normal text-[40px] leading-[125%] whitespace-nowrap">
                   {word.en}
                 </span>
-                <span className="font-pretendard font-medium text-[45px] leading-[100%]">
+                <span className="font-pretendard font-medium text-[34px] leading-[100%] whitespace-nowrap">
                   {word.ko}
                 </span>
               </div>
