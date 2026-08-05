8931eed1d45cc03ff44c37f25e4bb20ef21b3c13
iamjinpark <wldls7754@gmail.com>
2026-08-05 15:12:24 +0900

    refactor(mypage): #10 MyPage를 컴포넌트 조립으로 정리
    - **구현**
      - 인라인 마크업 160줄을 제거하고 ProfileSection / RecentMakeIt /
        StatsChart 조립과 데이터 주입만 담당하도록 축소

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,5 +1,21 @@
-function MyPage() {
-  return <div>MyPage</div>;
-}
+import ProfileSection from "@/components/myPage/ProfileSection";
+import RecentMakeIt from "@/components/myPage/RecentMakeIt";
+import StatsChart from "@/components/myPage/StatsChart";
+import { MY_PAGE_RECENT, MY_PAGE_STATS, MY_PAGE_USER } from "@/api/dummyData";
 
-export default MyPage;
+export default function MyPage() {
+  // TODO: 서버 연동 후 mock 데이터를 실제 응답으로 교체
+  const user = MY_PAGE_USER;
+  const recent = MY_PAGE_RECENT;
+  const stats = MY_PAGE_STATS;
+
+  return (
+    <div className="flex flex-col border-y border-black lg:min-h-[530px] lg:flex-row">
+      <ProfileSection user={user} />
+      <section className="flex min-w-0 flex-1 flex-col">
+        <RecentMakeIt post={recent} />
+        <StatsChart stats={stats} />
+      </section>
+    </div>
+  );
+}
